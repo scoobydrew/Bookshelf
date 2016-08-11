151565de7a8108b3ebc0f113675bd19b5d8d1704
Drew Smith <drewcsmith3@gmail.com>
2016-08-10 23:43:39 -0500

    Getting and displaying a list of books with our first service.

--- Bookshelf/App/Scripts/ListController.js
+++ Bookshelf/App/Scripts/ListController.js
@@ -1,6 +1,13 @@
 ﻿(function (app) {
-    let ListController = function () {
-        this.message = "Hello World!";
-    }; // Create the controller
-    app.controller("ListController", ListController); // Then register it to the app
+    let ListController = function ($scope, $http) {
+        let self = this; // we need to assign this to self, otherwise it will be out of scope when the http promise returns.
+        $http.get("/api/books")
+             .then(function (response) {
+                 self.books = response.data;
+             });
+    };
+    ListController.$inject = ['$scope', '$http'];
+    // Angular will automatically be able to inject parameters to the controller based on the parameter name.
+    // But using $inject is best practice because it is safe against minifiers that will shorten variable names but not string literals.
+    app.controller("ListController", ListController);
 }(angular.module("bookShelf")));
